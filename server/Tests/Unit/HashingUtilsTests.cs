b227b13302fbd70cb9fb53d8fda6e162ab3f87fa
Abdurahman Haji Abdi <abdulrahmanxambali@gmail.com>
2026-02-12 17:42:24 +0100

    HashingUtiltsTests init

---
+++ server/Tests/Unit/HashingUtilsTests.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Interfaces.Utility;
+using Infrastructure.Utils;
+
+namespace Unit;
+
+[Collection("Utils Collection")]
+public class HashingUtilsTests(IHashingUtils hashingUtils)
+{
+
+    [Fact]
+    public Task ShouldCreatePasswordHash_AndVerify()
+    {
+        //Arrange
+        var password = "password";
+        //Act
+        hashingUtils.CreatePasswordHash(password, out var passwordHash, out var passwordSalt);
+
+        //Assert
+        Assert.True(hashingUtils.VerifyPasswordHash(password, passwordHash, passwordSalt));
+        
+        return Task.CompletedTask;
+    }
+}
